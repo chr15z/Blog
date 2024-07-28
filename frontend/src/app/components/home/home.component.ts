77cfe3bfb3cd20790d33b3ac71d9b7ad264e02bb
chr15z <chrisztlr@gmail.com>
2024-07-28 17:13:47 +0200

    styling

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -16,13 +16,14 @@ export class HomeComponent implements OnInit {
     const first = new HomePageSectionDTO();
     first.title = "Cracking the Code";
     first.subtitle = "Sudoku Solving Algorithms: From Basic Strategies to Complex Computational Methods";
-    first.image = "assets/images/sudoku3.jpeg";
+    first.image = "assets/images/sudoku.jpeg";
 
     const second = new HomePageSectionDTO();
     second.title = "Behind the Scenes of Ticketline";
     second.subtitle = "Developing a Cutting-Edge Ticketing Platform for Concerts, Movies, and More";
-    second.image = "assets/images/ticket2.jpg";
-    this.homePageContent = [first, second]
+    second.image = "assets/images/ticket.jpg";
+
+    this.homePageContent = [second, first];
   }
 
   ngOnInit() {
