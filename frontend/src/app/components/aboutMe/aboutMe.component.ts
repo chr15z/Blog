f3f31972d98d863122b842178443fbc7690e81ab
chr15z <chrisztlr@gmail.com>
2024-10-15 23:10:36 +0200

    home needs to be revised

--- frontend/src/app/components/aboutMe/aboutMe.component.ts
+++ frontend/src/app/components/aboutMe/aboutMe.component.ts
@@ -10,8 +10,43 @@ export class AboutMeComponent implements OnInit {
   constructor() {
   }
 
-  ngOnInit() {
+  roles: string[] = [
+    'I am a web developer.',
+    'I am a student.',
+    'Check out my CV.',
+  ];
+  currentRoleIndex: number = 0;
+  currentRole: string = this.roles[0];
+
+  ngOnInit(): void {
+    this.startRoleRotation();
   }
 
+  startRoleRotation(): void {
+    setInterval(() => {
+      this.currentRoleIndex = (this.currentRoleIndex + 1) % this.roles.length;
+      this.currentRole = this.roles[this.currentRoleIndex];
+    }, 2000);
+  }
+
+
+  downloadPdf() {
+    // URL zur PDF-Datei (entweder lokal oder auf einem Server gehostet)
+    const pdfUrl = 'assets/CV.pdf';  // Wenn die Datei lokal im assets-Ordner liegt
+    const pdfName = 'CV.pdf';  // Name der herunterzuladenden Datei
+
+    // Erstelle einen unsichtbaren Link und löse den Download aus
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = pdfName;
+    link.click();
+  }
+
+  showPdf() {
+    const pdfUrl = 'assets/CV.pdf';  // Lokale oder externe PDF-Datei
+
+    // Öffne das PDF in einem neuen Tab/Fenster
+    window.open(pdfUrl, '_blank');
+  }
 
 }
