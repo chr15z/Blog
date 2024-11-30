a5a908bbe89560c45d5ccbad1e262b4b105836c1
chr15z <chrisztlr@gmail.com>
2024-11-30 12:53:53 +0100

    header and new hero section

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {NotFoundComponent} from "./components/notFound/notFound.component";
 import {SourcesComponent} from "./components/sources/sources.component";
-import {PAAComponent} from "./components/pAA/pAA.component";
 import {AboutMeComponent} from "./components/aboutMe/aboutMe.component";
 import {SudokuComponent} from "./components/sudoku/sudoku.component";
 
@@ -11,7 +10,6 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'notFound', component: NotFoundComponent},
   {path: 'sources', component: SourcesComponent},
-  {path: 'pAA', component: PAAComponent},
   {path: 'aboutMe', component: AboutMeComponent},
   {path: 'sudoku', component: SudokuComponent},
 
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -18,18 +18,29 @@ export class HomeComponent implements OnInit {
     first.image = "assets/images/sudoku.jpeg";
     first.id = 1;
     first.link = "/sudoku";
-    const second = new HomePageSectionDTO();
 
+    const second = new HomePageSectionDTO();
     second.title = "Behind the Scenes of Ticketline";
     second.subtitle = "Developing a Cutting-Edge Ticketing Platform for Concerts, Movies, and More";
     second.image = "assets/images/ticket2.jpg";
     second.id = 2;
     second.link = "/notFound";
 
-    this.homePageContent = [second, first];
+    const third = new HomePageSectionDTO();
+    third.title = "MyJourney";
+    third.subtitle = "Organise your health stress-free: Keep an eye on appointments, vital signs and preventive medical check-ups. Benefit from reminders, gamification and a personal overview.";
+    third.image = "assets/video/myJourney.mp4"; // DaVinci Resolve um den hintergrund transparent zu machen
+    third.id = 3;
+    third.link = "/notFound";
+
+
+    this.homePageContent = [first, second, third];
   }
 
   ngOnInit() {
+  }
+  isVideo(filePath: string): boolean {
+    return filePath.endsWith('.mp4') || filePath.endsWith('.webm') || filePath.endsWith('.ogg');
   }
 
 
