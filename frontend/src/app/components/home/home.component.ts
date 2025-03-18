8ae498a7d0db9c29397d89332e444bd73b60bcb6
chr15z <chrisztlr@gmail.com>
2025-03-18 10:13:10 +0100

    added ticketLine

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
     second.subtitle = "Developing a Cutting-Edge Ticketing Platform for Concerts, Movies, and More";
     second.image = "assets/images/ticket2.jpg";
     second.id = 2;
-    second.link = "/ticketLine";
+    second.link = "/notFound";
 
     const third = new HomePageSectionDTO();
     third.title = "Cracking the Code";
--- frontend/src/app/components/pTicketLine/ticketLine.component.ts
+++ frontend/src/app/components/pTicketLine/ticketLine.component.ts
@@ -7,8 +7,28 @@ import { HomePageSectionDTO } from '../../dtos/homePageSectionDTO';
   styleUrls: ['./ticketLine.component.scss']
 })
 export class TicketLineComponent implements OnInit {
+  images?: string[];
 
   constructor() {
+        this.images = [
+          "assets/images/sepm/sepm1.png",
+          "assets/images/sepm/sepm2.png",
+          "assets/images/sepm/sepm3.png",
+          "assets/images/sepm/sepm4.png",
+          "assets/images/sepm/sepm5.png",
+          "assets/images/sepm/sepm6.png",
+          "assets/images/sepm/sepm7.png",
+          "assets/images/sepm/sepm8.png",
+          "assets/images/sepm/sepm9.png",
+          "assets/images/sepm/sepm10.png",
+          "assets/images/sepm/sepm11.png",
+          "assets/images/sepm/sepm12.png",
+          "assets/images/sepm/sepm13.png",
+          "assets/images/sepm/sepm14.png",
+          "assets/images/sepm/sepm15.png",
+          "assets/images/sepm/sepm16.png",
+          "assets/images/sepm/sepm17.png",
+        ];
   }
 
   ngOnInit() {}
