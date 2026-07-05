921edca53a13e17e5faa3d41a0d82af115b9a042
chr15z <chrisztlr@gmail.com>
2026-07-05 20:20:24 +0200

    removed header and footer from /homeDashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import {BlurService} from "./services/blur.service";
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { BlurService } from "./services/blur.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CZ Blog ';
-  constructor(private readonly dropdownService: BlurService) { }
   isBlurred = false;
+  hideChrome = false;
+
+  constructor(
+    private readonly dropdownService: BlurService,
+    private readonly router: Router
+  ) { }
+
   ngOnInit(): void {
     this.dropdownService.isBlurred.subscribe(state => {
       this.isBlurred = state;
+    });
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.hideChrome = event.urlAfterRedirects.startsWith('/homeDashboard');
     });
   }
 }
